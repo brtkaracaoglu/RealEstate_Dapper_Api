b1082f31197d96adbd8fa7ccbcf4b53418566c88
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-01-23 11:16:11 +0300

    Bağlantı Kanfigürasyonları ve Repositoryler

---
+++ RealEstate_Dapper_Api/Models/DapperContext/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RealEstate_Dapper_Api.Models.DapperContext
+{
+    public class Context
+    {
+        //Bağlantı Konfigürasyonu
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public Context(IConfiguration configuration)// Constructor
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("connection"); //Baglantı adresi almak
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);  // Sqllin ormsini Microsotf data sql client ile calısılacak
+
+
+
+        
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+
+    //İnterface, bir sınıfın bir veya birden çok arayüzü uygulamasına olanak tanır.
+    //Bir sınıf bir arayüzü uyguladığında, o arayüzde tanımlanan tüm yöntemleri (metodları) sınıf içinde implemente etmek zorundadır.
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+    }
+}
