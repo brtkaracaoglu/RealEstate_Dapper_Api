b40e9ddd01e7d8cef07ba74d87e62b940ee63e3d
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-01-30 15:02:12 +0300

    Veri Setleri ve Bazı Düzenlemeler yapıldı.

--- RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
@@ -26,7 +26,7 @@ namespace RealEstate_Dapper_Api.Repositories.ProductRepository
 
         public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
         {
-            string query = "SELECT ProductID, Title, Price, City, District, CategoryName FROM Product INNER JOIN Category ON Product.ProductCategory = Category.CategoryID";
+            string query = "SELECT ProductID, Title, Price, City, District, CategoryName,CoverImage,Type,Address FROM Product INNER JOIN Category ON Product.ProductCategory = Category.CategoryID";
             using (var connection = _context.CreateConnection())
             {
                 //dapper kullanılan yer
