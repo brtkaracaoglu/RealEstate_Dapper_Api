41c7d1006a18c8e6c5da3fdfc5817fa2bf5584f6
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-01-31 17:26:38 +0300

    Api ile Tek Satır Veri Consume Etme

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/I_DefaultWhoWeAreComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public interface I_DefaultWhoWeAreComponentPartial
+    {
+        Task<IViewComponentResult> InvokeAsync();
+    }
+}
