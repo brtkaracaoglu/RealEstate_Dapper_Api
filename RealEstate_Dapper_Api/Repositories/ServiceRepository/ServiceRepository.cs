a2f69ab7e5591c7087d2345bfa683bfcea2fb81a
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-01 09:54:26 +0300

    Service Api

---
+++ RealEstate_Dapper_Api/Repositories/ServiceRepository/ServiceRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.ServicesDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        private readonly Context _context;
+
+        public ServiceRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void CreateService(CreateServiceDto serviceDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteService(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultServiceDto>> GetAllServiceAsync()
+        {
+            string query = "Select * From Service";
+            using (var connection = _context.CreateConnection())
+            {
+                //dapper kullanılan yer
+                var values = await connection.QueryAsync<ResultServiceDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public Task<GetByIDServiceDto> GetService(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateService(UpdateServiceDto serviceDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
