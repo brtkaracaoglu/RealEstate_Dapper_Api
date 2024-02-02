69eb4cc26d55d41d30189c863cf8fc0c96ba2f2a
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-02 16:06:04 +0300

    Admin Panelinde Kategori Apinin Consume Edilmesi.

---
+++ RealEstate_Dapper_UI/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.CategoryDtos
+
+{
+
+    // Dtos data transfer object  uygulama katmanları arasında veriyi transfer etmek için kullanılır.
+    public class CreateCategoryDto
+    {
+        public string CategoryName { get; set; }    
+    }
+}
