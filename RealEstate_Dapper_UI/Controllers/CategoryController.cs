55474cfe73e5839a743b13ed665c5456fb8c342c
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-05 11:07:38 +0300

    Api Consume ile Yeni Kategori Eklenmesi

--- RealEstate_Dapper_UI/Controllers/CategoryController.cs
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RealEstate_Dapper_UI.Dtos.CategoryDtos;
+using System.Text;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
@@ -16,12 +17,31 @@ namespace RealEstate_Dapper_UI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44330/api/BottomGrids");
+            var responseMessage = await client.GetAsync("https://localhost:44330/api/Categories");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient(); // ekleme ve güncelleme yaparken SerializeObject kullanılacaktır
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "aplication/json");
+            var responseMessage = await client.PostAsync("https://localhost:44330/api/Categories", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
