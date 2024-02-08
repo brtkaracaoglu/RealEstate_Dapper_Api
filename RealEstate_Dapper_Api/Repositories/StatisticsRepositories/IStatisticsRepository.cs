2c33628fe7f49d83b9e57cc90a52b209c350215c
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-08 11:18:25 +0300

    İstatislik İşlemleri

---
+++ RealEstate_Dapper_Api/Repositories/StatisticsRepositories/IStatisticsRepository.cs
@@ -0,0 +1,24 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.StatisticsRepositories
+{
+    public interface IStatisticsRepository
+    {
+        int CategoryCount();
+        int ActiveCategoryCount();
+        int PassiveCategoryCount();
+        int ProductCount();
+        int ApartmentCount();
+        string EmployeeNameByMaxProductCount();
+        string CategoryNameByMaxProductCount();
+        decimal AverageProductPriceByRent();
+        decimal AverageProductPriceBySale();
+        string CityNameByMaxProductCount();
+        int DifferentCityCount();
+        decimal LastProductPrice();
+        string NewestBuildingYear();
+        string OldestBuildingYear();
+        int ActiveEmployeeCount();
+        int AvereageRoomCount();
+    }
+}
