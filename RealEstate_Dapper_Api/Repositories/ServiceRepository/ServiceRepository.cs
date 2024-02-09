85f71bd8802b96321c6cb35f5fe1d3c0ff9df573
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-09 16:14:48 +0300

    Service Api Tamamlanması

--- RealEstate_Dapper_Api/Repositories/ServiceRepository/ServiceRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ServiceRepository/ServiceRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Dapper;
-using RealEstate_Dapper_Api.Dtos.CategoryDtos;
 using RealEstate_Dapper_Api.Dtos.ServicesDtos;
 using RealEstate_Dapper_Api.Models.DapperContext;
 
@@ -8,20 +7,31 @@ namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
     public class ServiceRepository : IServiceRepository
     {
         private readonly Context _context;
-
         public ServiceRepository(Context context)
         {
             _context = context;
         }
-
-        public void CreateService(CreateServiceDto serviceDto)
+        public async void CreateService(CreateServiceDto createServiceDto)
         {
-            throw new NotImplementedException();
+            string query = "insert into Service (ServiceName,ServiceStatus) values (@serviceName,@serviceStatus)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceName", createServiceDto.ServiceName);
+            parameters.Add("@serviceStatus", true);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
         }
 
-        public void DeleteService(int id)
+        public async void DeleteService(int id)
         {
-            throw new NotImplementedException();
+            string query = "Delete From Service Where ServiceID=@serviceID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
         }
 
         public async Task<List<ResultServiceDto>> GetAllServiceAsync()
@@ -29,20 +39,35 @@ namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
             string query = "Select * From Service";
             using (var connection = _context.CreateConnection())
             {
-                //dapper kullanılan yer
                 var values = await connection.QueryAsync<ResultServiceDto>(query);
                 return values.ToList();
             }
         }
 
-        public Task<GetByIDServiceDto> GetService(int id)
+        public async Task<GetByIDServiceDto> GetService(int id)
         {
-            throw new NotImplementedException();
+            string query = "Select * From Service Where ServiceID=@serviceID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIDServiceDto>(query, parameters);
+                return values;
+            }
         }
 
-        public void UpdateService(UpdateServiceDto serviceDto)
+        public async void UpdateService(UpdateServiceDto updateServiceDto)
         {
-            throw new NotImplementedException();
+            string query = "Update Service Set ServiceName=@serviceName,ServiceStatus=@serviceStatus where ServiceID=@serviceID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceName", updateServiceDto.ServiceName);
+            parameters.Add("@serviceStatus", updateServiceDto.ServiceStatus);
+            parameters.Add("@serviceID", updateServiceDto.ServiceID);
+
+            using (var connectiont = _context.CreateConnection())
+            {
+                await connectiont.ExecuteAsync(query, parameters);
+            }
         }
     }
 }
