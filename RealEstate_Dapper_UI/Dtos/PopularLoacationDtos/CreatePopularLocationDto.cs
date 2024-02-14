c165fa59fb4dfe968952bcc3674a4ef1e5c8b420
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-14 14:32:00 +0300

    Popüler Lokasyonlar Api ve Consume

---
+++ RealEstate_Dapper_UI/Dtos/PopularLoacationDtos/CreatePopularLocationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.PopularLoacationDtos
+{
+    public class CreatePopularLocationDto
+    {
+        public string CityName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
