2c11afbbb378c7289eb3fee05ef439fc4214552e
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-20 13:05:44 +0300

    Dashboard İşlemleri 1

---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashBoardLast5ProductComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashBoardLast5ProductComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DashBoardLast5ProductComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();  
+        }
+    }
+}
