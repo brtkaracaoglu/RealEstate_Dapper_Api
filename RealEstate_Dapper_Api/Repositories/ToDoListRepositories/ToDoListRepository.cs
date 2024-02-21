c1cf97584eabdb11c822f62dbb1013eeeeb3fb17
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2024-02-21 14:41:34 +0300

    Dashboardda ToDoList Kısmı

---
+++ RealEstate_Dapper_Api/Repositories/ToDoListRepositories/ToDoListRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ToDoListDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ToDoListRepositories
+{
+    public class ToDoListRepository : IToDoListRepository
+    {
+        private readonly Context _context;
+
+        public ToDoListRepository(Context context)
+        {
+            _context = context;
+        }
+        public async void CreateToDoList(CreateToDoListDto ToDoListDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void DeleteToDoList(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultToDoListDto>> GetAllToDoListAsync()
+        {
+            string query = "Select * From ToDoList";
+            using (var connection = _context.CreateConnection())
+            {
+                //dapper kullanılan yer
+                var values = await connection.QueryAsync<ResultToDoListDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIDToDoListDto> GetToDoList(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void UpdateToDoList(UpdateToDoListDto ToDoListDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RealEstate_Dapper_UI/ViewComponents/Dashboard/_DasboardToDoListComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DasboardToDoListComponentPartial.cs
@@ -1,24 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using RealEstate_Dapper_UI.Dtos.ProductDtos;
+using RealEstate_Dapper_UI.Dtos.ToDoListDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
 {
-    public class _DashboardLast5ProductComponentPartial : ViewComponent
+    public class _DasboardToDoListComponentPartial:ViewComponent
     {
+
         private readonly IHttpClientFactory _httpClientFactory;
-        public _DashboardLast5ProductComponentPartial(IHttpClientFactory httpClientFactory)
+        public _DasboardToDoListComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44330/api/Products/Last5ProductList");
+            var responseMessage = await client.GetAsync("https://localhost:44330/api/ToDoLists");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultLast5ProductWithCategoryDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultToDoListDto>>(jsonData);
                 return View(values);
             }
             return View();
